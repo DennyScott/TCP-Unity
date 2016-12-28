d24a8b064cf511a6a5f4b55c8841398651f288be
Denny Scott <dennyscott301@gmail.com>
2016-12-28 00:15:40 -0600

    Initial commit of working tcp connection

---
+++ Pulse/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pulse
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Start the server
+            TcpHelper.StartServer(5678);
+            TcpHelper.Listen(); // Start listening.
+        }
+    }
+}
